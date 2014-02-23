afa736eff360d7a7a4eaaf5163a801411e42f580
Colin Sullivan <colin@colin-sullivan.net>
2014-02-22 17:20:31 -0800

    video js 4.4.1 loading from local files instead of CDN.

--- package.js
+++ package.js
@@ -1,7 +1,21 @@
 Package.describe({
-      summary: "A smart package that makes it easy to use Video.js"
+  summary: "A smart package that makes it easy to use Video.js"
 });
 
 Package.on_use(function (api) {
-    api.add_files('video-js-load.js', 'client');
+  var path = Npm.require("path");
+
+  // make static files available
+  api.add_files(path.join("lib", "video-js", "video-js.swf"), "client");
+  api.add_files(path.join("lib", "video-js", "font", "vjs.eot"), "client");
+  api.add_files(path.join("lib", "video-js", "font", "vjs.svg"), "client");
+  api.add_files(path.join("lib", "video-js", "font", "vjs.ttf"), "client");
+  api.add_files(path.join("lib", "video-js", "font", "vjs.woff"), "client");
+
+  api.add_files(path.join("lib", "video-js", "video-js.css"), "client");
+  api.add_files(path.join("lib", "video-js", "video.dev.js"), "client");
+
+  // needs to go last, after videojs has loaded, to set the path to the
+  // static files
+  api.add_files(path.join("lib", "video-js-config.js"), "client");
 });
